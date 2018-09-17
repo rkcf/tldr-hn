8750626aec68307515770f4e98204b16ce346a4b
rkcf <rkcf@rkcf.me>
2018-09-17 12:36:23 -0700

    add pagination of stories

--- tldrhn/__init__.py
+++ tldrhn/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,8 +24,24 @@ class Position(db.Model):
 @app.route('/')
 def index():
     """ Front page view """
+    # Get page request arg
+    page = request.args.get('p', 1, type=int)
+
     # Get stories
     query = db.session.query(Story).join(Position, Story.id == Position.id)
-    stories = query.order_by(Position.position).all()
+    stories = query.order_by(Position.position).paginate(page, 10, False)
 
-    return render_template('index.html', stories=stories)
+    # Generate pagination links
+    if stories.has_next:
+        next_page = url_for('index', p=stories.next_num)
+    else:
+        next_page = None
+    if stories.has_prev:
+        prev_page = url_for('index', p=stories.prev_num)
+    else:
+        prev_page = None
+
+    return render_template('index.html',
+                           stories=stories.items,
+                           next_page=next_page,
+                           prev_page=prev_page)
