1b95847c4d8476a22d3c563b7f4bd942f368d26c
rkcf <rkcf@rkcf.me>
2018-09-17 22:12:32 -0700

    add text summarization

--- tldrhn/summarize.py
+++ tldrhn/summarize.py
@@ -1,4 +1,6 @@
 from hackernews import HackerNews
+from newspaper import Article
+from newspaper.article import ArticleException
 from . import db
 from . import Story
 from . import Position
@@ -6,7 +8,18 @@ from . import Position
 
 def gen_summary(url):
     """ Generate summary given story url """
-    return "Sit non provident perspiciatis doloribus omnis. Voluptates qui vel"
+    article = Article(url)
+    try:
+        # Get text
+        article.download()
+        article.parse()
+    except ArticleException:
+        return "Unable to generate summary"
+
+    # Tokenize text
+    article.nlp()
+    # Generate summary
+    return article.summary
 
 
 def main():
@@ -17,8 +30,8 @@ def main():
     front_page_list = []
 
     for story in stories:
-        # Only continue if not a job and not in db
-        if story.item_type != 'job':
+        # Only continue if not a job, or ask (ie external link) and not in db
+        if story.url:
             # Put story id into front page list
             front_page_list.append(story.item_id)
             if Story.query.filter_by(id=story.item_id).first() is None:
